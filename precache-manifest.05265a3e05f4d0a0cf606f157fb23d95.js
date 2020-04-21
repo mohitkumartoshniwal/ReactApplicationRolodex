a248bb51cc84d60f7d0161a52fa29c3fd5177fd0
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-21 20:21:18 +0530

    Updates

---
+++ precache-manifest.05265a3e05f4d0a0cf606f157fb23d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5ef9bb4eb4ee21ab571d6daa3ab4f5",
+    "url": "/ReactApplicationRolodex/index.html"
+  },
+  {
+    "revision": "2ab3460d2ef6c537e09b",
+    "url": "/ReactApplicationRolodex/static/css/main.05e175cf.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/ReactApplicationRolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApplicationRolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab3460d2ef6c537e09b",
+    "url": "/ReactApplicationRolodex/static/js/main.837e45ce.chunk.js"
+  },
+  {
+    "revision": "69c2a08da12ace71221d",
+    "url": "/ReactApplicationRolodex/static/js/runtime-main.986d06ec.js"
+  }
+]);
